ff8db39b1a548cddbac1dba5d7bfb41e61caf6e5
PhyoThihaOo <phyothihaoo.pto@gmail.com>
2026-06-16 14:41:46 -0400

    step1.2 done

--- TechTelantPipeline_R/Assignements/rsvp-manager/src/App.jsx
+++ TechTelantPipeline_R/Assignements/rsvp-manager/src/App.jsx
@@ -4,10 +4,21 @@ import "./App.css";
 export default function App() {
   const [name, setName] = useState("");
 
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
   return (
     <div>
       <h1>RSVP Practice</h1>
-      <input type="text" />
+      <input type="text" onChange={handleNameChange} value={name} />
+      <p>Name: {name}</p>
     </div>
   );
 }
+
+/**
+ * Controlled input - reacts owns the value. The input just display it
+ * value ={name}
+ * onChange - an event that fires on every keystroke. The event object e has e.target.value, what is what the user just typed
+ */

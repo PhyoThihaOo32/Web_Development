3f313e3d3b70daa3adb94ff4813031a07f72100f
PhyoThihaOo <phyothihaoo.pto@gmail.com>
2026-06-25 13:19:31 -0400

    part7 added recipe-api

--- TechTelantPipeline_R/Assignements/9_recipes-api/app.js
+++ TechTelantPipeline_R/Assignements/9_recipes-api/app.js
@@ -1,6 +1,7 @@
 const PORT = 3000;
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const router = require("./api/index");
 
 // // middleware function that logs the request's method and URL (req.method, req.originalUrl) to the console.
@@ -12,6 +13,7 @@ const router = require("./api/index");
 const app = express();
 app.use(express.json());
 // app.use(logger);
+app.use(cors());
 app.use(morgan("dev"));
 app.use("/api", router);
 

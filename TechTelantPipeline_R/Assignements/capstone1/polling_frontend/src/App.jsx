55df5f02bdead559e3d23f036fb539f706ec8b01
PhyoThihaOo <phyothihaoo.pto@gmail.com>
2026-07-21 13:52:40 -0400

    added frontend framework/ And HomePage

--- TechTelantPipeline_R/Assignements/capstone1/polling_frontend/src/App.jsx
+++ TechTelantPipeline_R/Assignements/capstone1/polling_frontend/src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import PollDetails from "./pages/PollDetails";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <h1>Poll App</h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/polls/:id" element={<PollDetails />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ TechTelantPipeline_R/Assignements/capstone1/polling_frontend/src/pages/PollDetails.jsx
@@ -0,0 +1,9 @@
+function PollDetails() {
+  return (
+    <div>
+      <h1>Poll Details</h1>
+    </div>
+  );
+}
+
+export default PollDetails;
